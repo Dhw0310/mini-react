0f1385b20b1067e9a4ac19e98b9224bf025e3227
du <dhw0310life@163.com>
2024-01-14 23:20:30 +0800

    feat: 实现fiber

--- v01/vite-runner/core/React.js
+++ v01/vite-runner/core/React.js
@@ -59,20 +59,88 @@ function createElement (type, props, ...children) {
 
 
 function render (el, container) {
-  const dom = el.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(el.type)
-
-  Object.keys(el.props).forEach(key => {
-    if (key !== "children") {
-      dom[key] = el.props[key]
+  nextWorkOfUnit = {
+    dom: container,
+    props: {
+      children: [el]
     }
-  })
-  const children = el.props.children
-  children.forEach(child => {
-    render(child, dom)
-  })
-  container.append(dom)
+  }
+  // const dom = el.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(el.type)
+
+  // Object.keys(el.props).forEach(key => {
+  //   if (key !== "children") {
+  //     dom[key] = el.props[key]
+  //   }
+  // })
+  // const children = el.props.children
+  // children.forEach(child => {
+  //   render(child, dom)
+  // })
+  // container.append(dom)
 }
 
+let nextWorkOfUnit = null
+function workLoop (deadline) {
+  let shouldYield = false
+  if (!shouldYield && nextWorkOfUnit) {
+    nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+function createDom (type) {
+  return type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(type)
+}
+
+function updateProps (dom, props) {
+  Object.keys(props).forEach(key => {
+    if (key !== "children") {
+      dom[key] = props[key]
+    }
+  })
+}
+function initChildren (fiber) {
+  // 3. 转换链表 设置好指针
+  let children = fiber.props.children
+  let prevChild = null
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      sibling: null,
+      dom: null
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevChild.sibling = newFiber
+    }
+    prevChild = newFiber
+  })
+}
+function performWorkOfUnit (fiber) {
+  // 1. 创建 DOM
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type))
+    fiber.parent.dom.append(dom)
+    // 处理 props
+    updateProps(dom, fiber.props)
+  }
+  initChildren(fiber)
+  // 4. 返回下一个要执行的任务
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  if (fiber.sibling) {
+    return fiber.sibling
+  }
+}
+
+requestIdleCallback(workLoop)
 const React = {
   render,
   createElement
