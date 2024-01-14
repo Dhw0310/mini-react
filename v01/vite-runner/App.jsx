3b1dd4e5157488c2e8eef0788c34488df70d6054
du <dhw0310life@163.com>
2024-01-14 22:15:42 +0800

    feat: 支持jsx

---
+++ v01/vite-runner/App.jsx
@@ -0,0 +1,10 @@
+import React from "./core/React.js"
+
+// function AppOne () {
+//   return <div id="hhh">hi-mini-react</div>
+// }
+// console.log(AppOne)
+// const App = React.createElement("div", { id: "app" }, "hi-", "mini-react")
+const App = <div>hi-mini-react</div>
+
+export default App
